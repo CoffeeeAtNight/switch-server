191914571e197a49a4b76561672539a69cc8bac3
small dog <grrr@bark.moon>
2025-07-06 13:20:14 +0200

    woof

--- arm_compile_remote.sh
+++ arm_compile_remote.sh
@@ -5,26 +5,30 @@ if [ $# -eq 0 ]
     echo "No arguments supplied - Needs arm32 source file and IP of compile server"
 fi
 
-DIR="arm_build_server"
-
 if [ ! -d "build" ]
 then
 	mkdir build
 fi
 
+UUID=$(uuidgen)
+DIR="arm_build_server/${UUID}/"
+
 # FOLDER STRUCT ON HOST
 # == /usr/bin/
 # ==== /arm_build_server
+
+# Create folder
+sshpass -p 'asm' ssh asm@pet "mkdir -p /usr/bin/${DIR}"
 
 # Move source file to compile server
 sshpass -p 'asm' scp $1 asm@pet:/usr/bin/$DIR/
 
 # Assemble and link source file
-sshpass -p 'asm' ssh asm@pet "arm-linux-gnueabihf-as -g -o /usr/bin/arm_build_server/peachykeen32.o /usr/bin/arm_build_server/m_peachykeen32.s"
-sshpass -p 'asm' ssh asm@pet "arm-linux-gnueabihf-ld -o /usr/bin/arm_build_server/peachykeen32 /usr/bin/arm_build_server/peachykeen32.o -Ttext=0x10000 --no-dynamic-linker -nostdlib"
+sshpass -p 'asm' ssh asm@pet "arm-linux-gnueabihf-as -g -o /usr/bin/${DIR}peachykeen32.o /usr/bin/${DIR}m_peachykeen32.s"
+sshpass -p 'asm' ssh asm@pet "arm-linux-gnueabihf-ld -o /usr/bin/${DIR}peachykeen32 /usr/bin/${DIR}peachykeen32.o -Ttext=0x10000 --no-dynamic-linker -nostdlib"
 
-# Move compiled files back
-sshpass -p 'asm' scp asm@pet:/usr/bin/$DIR/* build
+# Connect to server and bind stdout and stdin to local streams
+sshpass -p 'asm' ssh asm@pet "/usr/bin/${DIR}peachykeen32"
 
 # Clean
-sshpass -p 'asm' ssh asm@pet "rm -rf /usr/bin/$DIR/*" 
+sshpass -p 'asm' ssh asm@pet "rm -rf /usr/bin/$DIR" 
